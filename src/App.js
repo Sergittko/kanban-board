e41771e3d98ac27d0102c884526bbdf638881ff1
Sergittko <derov180214@gmail.com>
2023-04-14 01:20:51 +0300

    Initialize project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import style from "./App.module.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={style.app}>
+      <header className={style.app_header}>
+        <img src={logo} className={style.app_logo} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={style.app_link}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
